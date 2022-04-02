7956fbb0c7912665cc08cb95a05fc49bcb718566
ChristopherLMiller <chris@christopherleemiller.me>
2022-04-02 10:23:43 -0400

    add basic API guard

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { BasicAuthGuard } from './guards/basicAuth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -11,9 +12,13 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
+  // enable cors for all origins
   app.enableCors({
     origin: '*',
   });
+
+  // apply the global guards
+  app.useGlobalGuards(new BasicAuthGuard());
 
   await app.listen(3000);
 }
