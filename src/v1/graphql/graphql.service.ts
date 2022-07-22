67a7683b37fe8d178cc7e0d8a6e02a853fb4da39
ChristopherLMiller <chris@christopherleemiller.me>
2022-07-22 11:51:15 -0400

    feat: :sparkles: GraphQL temporary endpoint

---
+++ src/v1/graphql/graphql.service.ts
@@ -0,0 +1,52 @@
+import { HttpService } from '@nestjs/axios';
+import {
+  BadRequestException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { catchError, firstValueFrom, throwError } from 'rxjs';
+
+@Injectable()
+export class GraphqlService {
+  constructor(private http: HttpService) {}
+
+  handleError(error: any) {
+    switch (error.response.status) {
+      case HttpStatus.BAD_REQUEST:
+        return throwError(
+          () => new BadRequestException(error.response.data.errors[0].message),
+        );
+      case HttpStatus.UNAUTHORIZED:
+        return throwError(
+          () =>
+            new UnauthorizedException(error.response.data.errors[0].message),
+        );
+      default:
+        return throwError(() => new Error('unhandled exception'));
+    }
+  }
+
+  // Query the GraphQL API
+  query(query: string, variables?: any): Promise<any> {
+    try {
+      const response = this.http
+        .post(
+          'graphql',
+          { query, variables },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json',
+              Authorization: 'Bearer suckadeeznuts',
+            },
+          },
+        )
+        .pipe(catchError(this.handleError));
+      return firstValueFrom(response);
+    } catch (error) {
+      console.log('we caught an error');
+      throw new BadRequestException(error);
+    }
+  }
+}
